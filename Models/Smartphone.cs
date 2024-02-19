337c2323fbb8ca0cef15253f4628474cc095f8a6
Diego Parreiras <diegoparreiraswin@gmail.com>
2024-02-19 10:29:00 -0300

    Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,31 @@
+using System.Runtime.CompilerServices;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar(string numero)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para {numero}");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string numero)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação de {numero}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "5293963801", modelo: "11111", imei: "112233", memoria: 32);
+Smartphone iphone = new Iphone(numero: "8881234678", modelo: "222333", imei: "1309128", memoria: 128);
+
+Console.WriteLine("Smartphone Nokia;");
+nokia.Ligar(iphone.Numero);
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("_________________________");
+
+Console.WriteLine("Smartphone Iphone;");
+
+iphone.ReceberLigacao(nokia.Numero);
+iphone.InstalarAplicativo("Instagram");
+Console.WriteLine("_________________________");
